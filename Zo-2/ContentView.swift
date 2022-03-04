fb8b9d39c543b33be96147cf83baf0b250ae1c4e
sh862346 <sheralall@albany.edu>
2022-03-04 10:43:11 -0500

    Initial Commit

---
+++ Zo-2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Zo-2
+//
+//  Created by Brian Heralall on 3/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Zo-2/Zo_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Zo_2App.swift
+//  Zo-2
+//
+//  Created by Brian Heralall on 3/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Zo_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
